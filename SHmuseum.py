bb7b96af6da1bda49cb018463bb2cc27cc22e981
ytwu <torrence@mail.ustc.edu.cn>
2019-06-22 22:33:01 +0800

    RSS - Add MySQL support

--- SHmuseum.py
+++ SHmuseum.py
@@ -6,6 +6,7 @@ import requests
 import bs4
 import time
 import json
+import pymysql
 
 REQUEST_DELAY = 0.5 # second
 
@@ -37,13 +38,47 @@ r = s.get(HOST_URL, headers=login_header)
 login_header['Referer'] = WEB_URL
 login_header['X-Requested-With'] = 'XMLHttpRequest' 
 
+# Open MySQL connection
+with open('private-db.json') as f:
+  API_DB = json.load(f)['rss']
+  db = pymysql.connect('localhost',API_DB['USER'], API_DB['PASS'], 'rss')
+  p = db.cursor()
+
+def ConvertAPIResult(act):
+  cmd = "INSERT INTO info(title, abstract, url, source, info_time) VALUES ('%s', '%s','%s','%s', '%s')" % \
+    (act['activityName'] + ' ' + act['speaker'], \
+    act['activityContent'].replace("'","''"), \
+    'https://www.shanghaimuseum.net/education/show/show-content?activityId=' + act['activityNo'], \
+    '上海博物馆', \
+    act['activityDate'].replace('T',' '))
+  return cmd
+
+def InsertDB(act):
+  title = act['activityName'] + ' ' + act['speaker']
+  cmd = "SELECT * FROM info WHERE title='%s'" % title
+  if(p.execute(cmd) > 0):
+      print('[+] INFO existed - ' + title)
+      return False
+  else:  
+    print('[+] INFO NEWS found - ' + title)
+    try:
+      cmd = ConvertAPIResult(act)
+      p.execute(cmd)
+      db.commit()
+    except:
+      db.rollback()
+      raise
+
 for pageNo in range(1,10):
   time.sleep(REQUEST_DELAY)
   API_DATA['pageNo'] = pageNo
-  r = s.post(API_URL, headers=login_header, data=API_DATA)
+  r = s.post(API_URL, headers=login_header, data=API_DATA, timeout=2)
   RAW = json.loads(r.text)
   for act in RAW['activityList']:
+    InsertDB(act)
     #print(act['activityDate'] + ' ' + act['activityName'])
     if(act['activityStatus'] != '该活动预约人数已至上限。' and act['activityStatus'] != '该活动已结束。'):
       print(act['activityDate'] + ' ' + act['activityName']+' '+act['speaker']+'\n')
       print('> '+act['activityContent']+'\n')
+
+db.close()

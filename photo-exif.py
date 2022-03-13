54f9115332e2707d7a7eb6ee5b9d96f3e9283f83
yatowoo <torrence@mail.ustc.edu.cn>
2022-03-13 22:45:39 +0800

    Photo - Sorting exif

---
+++ photo-exif.py
@@ -0,0 +1,16 @@
+#!/bin/env python3
+
+import sys
+from PIL import Image
+
+with Image.open(sys.argv[1]) as img:
+  try:
+    exif = img._getexif()
+  except Exception:
+    print("Error")
+    exit()
+  if(exif is None):
+    exit()
+  cam = exif.get(272)
+  if(cam is not None):
+    print(cam)
